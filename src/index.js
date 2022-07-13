98c2884608b68750647212f50deffd5e28000737
BlackstarDamien <damianzylka@egmail.com>
2022-07-13 21:37:52 +0000

    #2 Create sample index.js module

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const jsx = (
+    <>
+        <h1>Checking</h1>
+    </>
+)
+
+ReactDOM.render(jsx, document.getElementById("root"))
